9e893f547998010cbce4ccbbce5e9291182f835d
Chirag <hi@chiragasarpota.com>
2023-08-23 22:52:17 +0400

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
-import Image from 'next/image'
+"use client"; // 👈 use it here
+
+import { useState } from "react";
+import { IconContext } from "react-icons";
+
+import {
+  FaHandshake,
+  FaCalculator,
+  FaFileContract,
+  FaPenRuler,
+  FaFileSignature,
+  FaHammer,
+  FaClipboardCheck,
+  FaHouseCircleCheck,
+} from "react-icons/fa6";
+import { FaHardHat, FaHeadset } from "react-icons/fa";
 
 export default function Home() {
+  const [selectedStep, setSelectedStep] = useState(1);
+  const [stepDetail, setStepDetail] = useState("INITIAL CONSULTATION");
+  const stepStyles = [
+    { transform: "rotate(0deg) translateY(-250%) rotate(0deg)" },
+    { transform: "rotate(36deg) translateY(-250%) rotate(-36deg)" },
+    { transform: "rotate(72deg) translateY(-250%) rotate(-72deg)" },
+    { transform: "rotate(108deg) translateY(-250%) rotate(-108deg)" },
+    { transform: "rotate(144deg) translateY(-250%) rotate(-144deg)" },
+    { transform: "rotate(180deg) translateY(-250%) rotate(-180deg)" },
+    { transform: "rotate(216deg) translateY(-250%) rotate(-216deg)" },
+    { transform: "rotate(252deg) translateY(-250%) rotate(-252deg)" },
+    { transform: "rotate(288deg) translateY(-250%) rotate(-288deg)" },
+    { transform: "rotate(324deg) translateY(-250%) rotate(-324deg)" },
+  ];
+  const detailsArray = [
+    "INITIAL CONSULTATION",
+    "PROJECT ASSESSMENT AND ESTIMATION",
+    "PROPOSAL AND CONTRACT",
+    "DESIGN PHASE",
+    "PERMITS AND APPROVALS",
+    "SITE PREPARTION",
+    "RENOVATION WORK",
+    "QUALITY CONTROL",
+    "COMPLETION AND HANDOVER",
+    "POST-RENOVATION SUPPORT",
+  ];
+  let iconStyles = { color: "white", fontSize: "2em" };
+
+  const iconsArray = [
+    <FaHandshake />,
+    <FaCalculator />,
+    <FaFileContract />,
+    <FaPenRuler />,
+    <FaFileSignature />,
+    <FaHardHat />,
+    <FaHammer />,
+    <FaClipboardCheck />,
+    <FaHouseCircleCheck />,
+    <FaHeadset />,
+  ];
+
+  const handleSelection = (index: string) => {
+    setSelectedStep(parseInt(index));
+    setStepDetail(detailsArray[parseInt(index) - 1]);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+      <div className="relative flex h-screen w-screen items-center justify-center">
+        {/* Main circle that shows details */}
+        <div className="z-10 flex flex-col h-36 w-36 md:h-52 md:w-52 xl:h-64 xl:w-64 items-center place-content-center justify-center text-sm md:text-xl font-medium text-center text-white	rounded-full bg-blue-500 p-8">
+          <IconContext.Provider
+            value={{
+              color: "white",
+              className: "text-lg md:text-2xl mb-4",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div>{iconsArray[selectedStep - 1]}</div>
+          </IconContext.Provider>
+          {stepDetail}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {/* Process steps */}
+        {stepStyles.map((style, index) => (
+          <div key={index} style={style} className="absolute">
+            <div
+              onClick={(e) =>
+                handleSelection(e.currentTarget.getAttribute("data-id") || "1")
+              }
+              onMouseEnter={(e) =>
+                handleSelection(e.currentTarget.getAttribute("data-id") || "1")
+              }
+              data-id={index + 1}
+              className={`flex h-12 w-12 md:h-16 md:w-16 xl:h-20 xl:w-20 items-center justify-center rounded-full text-center ${
+                selectedStep == index + 1 ? "bg-red-500" : "bg-gray-700"
+              }`}
+            >
+              <IconContext.Provider
+                value={{ color: "white", className: "text-lg md:text-2xl" }}
+              >
+                <div>{iconsArray[index]}</div>
+              </IconContext.Provider>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
-  )
+  );
 }
