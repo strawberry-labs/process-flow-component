0a9c163c5d8d6a0fcf9ab6ff66f9d0d989cb3d6f
Chirag <hi@chiragasarpota.com>
2023-08-24 13:28:48 +0400

    bug: smoothened animation

--- app/page.tsx
+++ app/page.tsx
@@ -37,12 +37,6 @@ export default function Home() {
   const [stepDetail, setStepDetail] = useState("INITIAL CONSULTATION");
   const [animationClass, setAnimationClass] = useState(true);
 
-  useEffect(() => {
-    setAnimationClass(false);
-    void document.documentElement.offsetWidth;
-    setAnimationClass(true);
-  }, [stepDetail]);
-
   const stepStyles = [
     { transform: "rotate(0deg) translateY(-400%) rotate(0deg)" },
     { transform: "rotate(36deg) translateY(-400%) rotate(-36deg)" },
@@ -110,6 +104,8 @@ export default function Home() {
 
   const handleSelection = (index: string) => {
     setAnimationClass(false);
+    void document.documentElement.offsetWidth;
+    setAnimationClass(true);
     setSelectedStep(parseInt(index));
     setStepDetail(detailsArray[parseInt(index) - 1]);
   };
